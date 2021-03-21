98f61cf21097db653bc5f4417b0198313b06439b
Atamurad <Atamurad.babakulyyev@gmail.com>
2021-03-21 00:40:53 -0400

    footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,24 @@ import './Footer.scss';
 function Footer() {
   return (
     <footer className="footer">
-      
+      <div class="row">
+        <div class="col-1-of-2">
+          <div class="footer__navigation">
+            <ul class="footer-list">
+              <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
+              <li class="footer__item"><a href="#" class="footer__link">Contact us</a></li>
+              <li class="footer__item"><a href="#" class="footer__link">Careers</a></li>
+              <li class="footer__item"><a href="#" class="footer__link">privacy policy</a></li>
+              <li class="footer__item"><a href="#" class="footer__link">terms</a></li>
+            </ul>
+          </div>
+        </div>
+          <div class="col-1-of-2">
+            <p class="footer__copyright">
+              Built my <a href="#" class="footer__link">Atamurad</a> for my project.Copyright &copy; by Atamurad Babakulyyev. as a software developer.
+            </p>
+          </div>
+        </div>
     </footer>
   )
 }
